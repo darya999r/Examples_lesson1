c8de1ab6e086877f181dadcbec5f492fe9bb9aad
Darya Nesterova <darya999r@mail.ru>
2022-08-29 15:41:42 +0300

    examples methodes task

--- Examples011_Methods/Program.cs
+++ Examples011_Methods/Program.cs
@@ -25,7 +25,7 @@ void Method2(string msg)
 void Method2b(string msg, int count)
 {
     int i = 0;
-    while(i<count)
+    while (i < count)
     {
         Console.WriteLine(msg);
         i++;
@@ -55,8 +55,8 @@ string Method4(int count, string c)
 {
     int i = 0;
     string result = String.Empty;
-    
-    while(i<count)
+
+    while (i < count)
     {
         result = result + c;
         i++;
@@ -67,3 +67,41 @@ string Method4(int count, string c)
 //Call the method:
 // string res = Method4(10, "abcdf");
 // Console.WriteLine(res);
+
+
+//Пример задачи
+//Дан текст. В тексте надо все пробелы заменить черточками, 
+//маленькие буквы "k" - большими "K", большие буквы "M" - маленькими "m".
+
+string text = "Meet my family. "
+            + "There are five of us – my parents, "
+            + "my elder brother, my baby sister and me. "
+            + "First, meet my mum and dad, Jane and Michael. "
+            + "My mum enjoys reading and my dad enjoys playing "
+            + "chess with my brother Ken. My mum is slim and rather tall.";
+
+//string s = "gwerty"
+//            0123...
+//s[3] -> r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+        
+    }
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+string newText1 = Replace(newText, 'k', 'K');
+// Console.WriteLine(newText1);
+// Console.WriteLine();
+string newText2 = Replace(newText1, 'M', 'm');
+Console.WriteLine(newText2);
