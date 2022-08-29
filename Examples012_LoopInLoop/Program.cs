da7d7e728d24f96f8c1abe65a5a751ab40c56000
Darya Nesterova <darya999r@mail.ru>
2022-08-29 14:32:23 +0300

    examples loop_in_loop

---
+++ Examples012_LoopInLoop/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 1 
+//Вывод таблицы умножения на экран.
+
+// for(int i = 2; i <= 10; i++)
+// {
+//     for(int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} * {j} = {i*j}");
+//     }
+//     Console.WriteLine();
+// }
+
+//Задача 2
+//Упорядочить массив от наименьшего числа к наибольшему.
+
+int [] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+void PrintArray(int [] array)
+{
+    int count = array.Length;
+    for(int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int [] array)
+{
+    for (int i = 0; i < array.Length-1; i++)
+    {
+        int minPosition = i;
+        for (int j = i+1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
